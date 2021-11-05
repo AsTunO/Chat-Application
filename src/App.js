b7d67cf23bfe02489965f1675b38e9f71461d685
Júlio Cesar <juliocesar27072004@outlook.com>
2021-11-05 15:23:57 -0300

    End

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import {ChatEngine} from 'react-chat-engine';
+import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
-
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '30df5162-8529-4ebc-bb86-2ccfa6b233d5';
+
 const App = () => {
-    return (
-        <ChatEngine 
-            height="100vh"
-            projectID="30df5162-8529-4ebc-bb86-2ccfa6b233d5"
-            userName="AsTunO"
-            userSecret="juliocesar27"
-            renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} /> }
-        />
-    );
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
 
 export default App; 
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,35 +1,34 @@
-
 const TheirMessage = ({ lastMessage, message }) => {
-    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+  const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
 
-    return (
-        <div className="message-row">
-        {isFirstMessageByUser && (
-            <div
-                className="message-avatar"
-                style={{ backgroundImage: `url(${message?.sender?.avatar})`}}
-            />
+  return (
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
+        />
+      )}
+      {message.attachments && message.attachments.length > 0
+        ? (
+          <img
+            src={message.attachments[0].file}
+            alt="message-attachment"
+            className="message-image"
+            style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+          />
+        )
+        : (
+          <div className="message" style={{ 
+            float: 'left', 
+            backgroundColor: '#CABCDC', 
+            marginLeft: isFirstMessageByUser ? '4px' : '48px' 
+          }}>
+            {message.text}
+          </div>
         )}
-        {message?.attachments?.length > 0
-            ? (
-            <img
-                src={message.attachments[0].file}
-                alt="message-attachment"
-                className="message-image"
-                style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
-            />
-            )
-            : (
-            <div className="message" style={{ 
-                float: 'left', 
-                backgroundColor: '#CABCDC', 
-                marginLeft: isFirstMessageByUser ? '4px' : '48px' 
-                }}>
-                {message.text}
-            </div>
-            )}
-        </div>
-    );
-    };
+    </div>
+  );
+};
 
 export default TheirMessage;
