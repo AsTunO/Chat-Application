5957dc7572cbc4739fbc8afd3e3c30180bc4ea7c
Júlio Cesar <juliocesar27072004@outlook.com>
2021-11-05 15:51:33 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import { ChatEngine } from 'react-chat-engine';
-
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = '30df5162-8529-4ebc-bb86-2ccfa6b233d5';
+const projectID = '6875f6d3-9ab5-460e-a15e-8a4a68e773b5';
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
 
   return (
-    <ChatEngine
-      height="100vh"
-      projectID={projectID}
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
-    />
-  );
+		<ChatEngine
+			height='100vh'
+			userName='Root'
+			userSecret='1234'
+			projectID='6875f6d3-9ab5-460e-a15e-8a4a68e773b5'
+		/>
+	);
 };
 
 export default App; 
