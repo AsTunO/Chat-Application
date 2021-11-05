20a7b012dafdac3fdfc2cd2b73f66c864dbf8543
Júlio Cesar <juliocesar27072004@outlook.com>
2021-11-05 14:12:51 -0300

    Chat Engine Connection

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {ChatEngine} from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="30df5162-8529-4ebc-bb86-2ccfa6b233d5"
+            userName="AsTunO"
+            userSecret="juliocesar27"
+        />
+    );
+}
+
+export default App; 
